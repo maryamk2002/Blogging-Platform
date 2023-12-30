921bddfd6d42de5d3dc6abf96615ccd62eca15d1
maryamk2002 <maryam.kamal.a155@gmail.com>
2023-12-30 20:35:10 +0500

    merged-faiza

---
+++ Blog_Platform/src/main/DisplayPost.java
@@ -0,0 +1,39 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class DisplayPost{
+    public String displayPost(int authorId) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/blogpost", "root", "");
+            PreparedStatement preparedStatement = con.prepareStatement("SELECT b.post_id,a.author_name, b.title, b.content FROM blogpost b JOIN author a ON b.author_id = a.author_id WHERE a.author_id = ?");
+            preparedStatement.setInt(1, authorId);
+            ResultSet rs = preparedStatement.executeQuery();
+
+
+            StringBuilder result = new StringBuilder();
+
+            while (rs.next()) {
+                
+                result.append("Post ID: ").append(rs.getInt("post_id")).append("\n")
+                        .append(" Author Name: ").append(rs.getString("author_name")).append("\n")
+                        .append(" Title: ").append(rs.getString("title")).append("\n")
+                        .append(" Content:").append(rs.getString("content")).append("\n\n");
+                       
+                
+            }
+
+            return result.toString();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return "Program couldn't be executed";
+    }
+}
---
+++ Blog_Platform/src/main/EditPost.java
@@ -0,0 +1,46 @@
+package main; 
+
+import java.sql.*;
+
+public class EditPost {
+
+    public String editPost(int authorId, int postId, String updatedTitle, String updatedContent) {
+        try {
+        	Class.forName("com.mysql.jdbc.Driver");
+        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/blogpost","root","");
+           
+            PreparedStatement selectStatement = connection.prepareStatement("SELECT title, content FROM blogpost WHERE post_id = ? AND author_id = ?");
+            PreparedStatement updateStatement = connection.prepareStatement("UPDATE blogpost SET title = ?, content = ? WHERE post_id = ? AND author_id = ?");
+
+            // Retrieve post details
+            selectStatement.setInt(1, postId);
+            selectStatement.setInt(2, authorId);
+            ResultSet resultSet = selectStatement.executeQuery();
+
+            if (resultSet.next()) {
+                // Post found, display current details
+                String currentTitle = resultSet.getString("title");
+                String currentContent = resultSet.getString("content");
+
+                // Get updated details from the user
+                updateStatement.setString(1, updatedTitle);
+                updateStatement.setString(2, updatedContent);
+                updateStatement.setInt(3, postId);
+                updateStatement.setInt(4, authorId);
+                int rowsAffected = updateStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    // Return a message with both current and updated details
+                    return "Post updated successfully!\nCurrent Title: " + currentTitle + "\nCurrent Content: " + currentContent;
+                } else {
+                    return "No matching post found for the given Author ID and Post ID.";
+                }
+            } else {
+                return "No matching post found for the given Author ID and Post ID.";
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return "Error occurred while updating the post.";
+        }
+    }
+}
